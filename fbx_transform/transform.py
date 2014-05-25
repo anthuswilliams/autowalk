662413eed4a8e0bb346890696cfd3b5fe05a0f3b
Anthus Williams <awilliams@instructure.com>
2014-05-25 17:25:31 -0600

    Refactor import logic and retrieve mesh from provided FBX file

---
+++ fbx_transform/transform.py
@@ -0,0 +1,52 @@
+import fbx, sys, os
+
+# Imports an FBX file containing a model, converts its units, and approximates certain physical constraints
+class FbxTransform:
+  def __init__(self, path):
+    if not os.path.isfile(path):
+        print "No such file: %s" % sys.argv[1]
+        return
+    self.file_path = os.path.abspath( sys.argv[1] )
+    self.manager = fbx.FbxManager.Create()
+    self.manager.SetIOSettings( fbx.FbxIOSettings.Create(self.manager, fbx.IOSROOT) )
+    (errcode, scene) = self.import_scene()
+    if errcode:
+        print "Error during FBX import: %s" % scene
+        return
+    self.scene = self.transform_scene(scene)
+    self.model = self.extract_model(scene)
+
+  def import_scene(self):
+    importer = fbx.FbxImporter.Create(self.manager, "")
+    if importer.Initialize(self.file_path) == False:
+        return (1, importer.GetStatus().GetErrorString())
+
+    scene = fbx.FbxScene.Create(self.manager, "default_scene")
+    importer.Import(scene)
+    importer.Destroy()
+    return (0, scene)
+
+  # should store these settings so we can export the result in the same format we received
+  def transform_scene(self, scene):
+    settings = scene.GetGlobalSettings()
+    if not settings.GetSystemUnit() == fbx.FbxSystemUnit.m:
+      fbx.FbxSystemUnit.m.ConvertScene(scene)
+    axis_reference = settings.GetAxisSystem()
+    # in the statements below, the 1 refers to positive direction
+    if axis_reference.GetUpVector() != (fbx.FbxAxisSystem.eYAxis, 1) \
+      or axis_reference.GetFrontVector() != (fbx.FbxAxisSystem.eParityOdd, 1) \
+      or axis_reference.GetCoorSystem() != fbx.FbxAxisSystem.eRightHanded:
+      fbx.FbxAxisSystem(fbx.FbxAxisSystem.eMayaYUp).ConvertScene(scene)
+
+    return scene
+
+  def extract_model(self, scene)
+    root_node = scene.GetRootNode()
+    if root_node.GetChildCount != 1:
+      raise "Multiple nodes in provided file. Which to use?"
+    model = root_node.GetChild(0)
+    if not isinstance(model.getNodeAttribute, fbx.FbxMesh):
+      raise "Node should be a mesh!"
+
+
+
--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-import fbx, sys, ode, os
+import fbx_transform, sys
 
-if len(sys.argv) < 2:
-    print >> sys.stderr, "Usage: python main.py /path/to/fbx/file"
-    sys.exit(2)
-if not os.path.isfile(sys.argv[1]):
-    sys.exit( "No such file: %s" % sys.argv[1] )
-
-manager = fbx.FbxManager.Create()
-manager.SetIOSettings( fbx.FbxIOSettings.Create(manager, fbx.IOSROOT) )
-importer = fbx.FbxImporter.Create(manager, "")
-
-b = importer.Initialize(os.path.abspath( sys.argv[1] ))
-if b == False:
-    sys.exit( importer.GetStatus().GetErrorString() )
-
-scene = fbx.FbxScene.Create(manager, "default_scene")
-
-importer.Import(scene)
-importer.Destroy()
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print >> sys.stderr, "Usage: python main.py /path/to/fbx/file"
+        exit(2)
+    fbx_transform.FbxTransform(sys.argv[1])
